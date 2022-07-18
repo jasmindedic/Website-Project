42fe377983fdbf5bb46c594cdb6a818334651f9f
Jasmin Dedic <jdedic1999@gmail.com>
2022-07-18 17:01:44 +0200

    Created a setIntervall feature for a paragraph

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 "use strict";
 
+ // Function that changes text every few seconds for the text under the header
+ const textChange = ["We dont do drama, we do basketball", "Heaven for every Basketball fan", "Enormous range of basketball stuff"];
  
+ let counter = 0;
+ let text = document.querySelector(".text-pic");
+ let inst = setInterval(change, 3000);
+
+ function change() {
+    text.innerHTML = textChange[counter];
+    counter++;
+    if (counter >= textChange.length){
+        counter = 0;
+        //clearInterval(inst);
+    }
+ }
+
+
