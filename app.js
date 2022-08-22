d546ae2d2e12ab91be0c53adae50bcf610466167
Jasmin Dedic <jdedic1999@gmail.com>
2022-08-22 09:26:45 +0200

    Added animations

--- app.js
+++ app.js
@@ -17,10 +17,39 @@
  }
 
 
-// Create function for shop button to direct to shopping page
+// Function for shop button to direct to shopping page
 const shoppingButton = document.querySelector(".shopping-btn");
-shoppingButton.addEventListener("click", function(){
+shoppingButton.addEventListener("click", e => {
     window.location.href = "shop-page.html";
 });
  
+/*
+// Get all add to cart buttons into varibles
+let blazerBtn = document.getElementById("blazer");
+let nike90Btn = document.getElementById("nike-90");
+let airForceBtn = document.getElementById("air-force");
+let jordan1Btn = document.getElementById("jordan-1");
 
+// Ul element
+let itemList = document.querySelector(".cart-list");
+
+// Add listener to all add to cart buttons
+blazerBtn.addEventListener("click", e =>{
+    console.log(e);
+
+
+
+    
+    let liBlazer = document.createElement("li");
+
+    let blazerPic = document.createElement("img")
+    blazerPic = window.location.href = "./images.shoe1";
+    liBlazer.appendChild(blazerPic);
+
+    // Append the li element to the Ul element
+    itemList.appendChild(liBlazer);
+   
+});
+
+
+*/
